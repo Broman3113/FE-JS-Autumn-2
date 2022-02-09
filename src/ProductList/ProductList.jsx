e933e12d8517c3762ec04aa23a69dd7c8e2ab9a1
Broman3113 <broman3113@gmail.com>
2022-02-09 14:39:34 +0200

    Add Lesson-2 prod stylized

--- src/ProductList/ProductList.jsx
+++ src/ProductList/ProductList.jsx
@@ -4,28 +4,30 @@ import Product from "./Product/Product";
 class ProductList extends Component {
     render() {
         return (
-            <table>
-                <caption>Click on any item to edit</caption>
-                <thead>
-                <tr>
-                    <td>№</td>
-                    <td>Product Name</td>
-                    <td>Category</td>
-                    <td>Color</td>
-                    <td>Actions</td>
-                </tr>
-                </thead>
-                <tbody>
-                {this.props.products.map((product, index) => <Product
-                    key={product.id}
-                    product={product}
-                    categories={this.props.categories}
-                    index={index}
-                    onDeleteProduct={this.props.onDeleteProduct}
-                    onEditProduct={this.props.onEditProduct}
-                />)}
-                </tbody>
-            </table>
+            <div className="productList">
+                <table>
+                    <caption>Click on any item to edit</caption>
+                    <thead>
+                    <tr>
+                        <td>№</td>
+                        <td>Product Name</td>
+                        <td>Category</td>
+                        <td>Color</td>
+                        <td>Actions</td>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {this.props.products.map((product, index) => <Product
+                        key={product.id}
+                        product={product}
+                        categories={this.props.categories}
+                        index={index}
+                        onDeleteProduct={this.props.onDeleteProduct}
+                        onEditProduct={this.props.onEditProduct}
+                    />)}
+                    </tbody>
+                </table>
+            </div>
         )
     }
 }
